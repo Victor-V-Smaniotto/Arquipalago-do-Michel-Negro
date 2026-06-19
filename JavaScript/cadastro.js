79f3fd998ba5a560d45599e663a8a5e820c6b24f
Augusto Cezar Morgenstern <augusto_schaefer@estudante.sesisenai.org.br>
2026-06-19 14:58:32 -0300

    js cadastro feito

--- JavaScript/cadastro.js
+++ JavaScript/cadastro.js
@@ -1,20 +1,7 @@
 const formulario = document.querySelector("#formulario");
-
-formulario.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const user = {
-        nome: document.querySelector("#nome").value,
-        raca: document.querySelector("#raca").value,
-        classe: document.querySelector("#classe").value
-    };
-
-    console.log(user);
-});
-
+const listaUsers = document.querySelector("#lista-users");
 
 const pontosR = document.querySelector("#pontos-restantes");
-
 const forca = document.querySelector("#val-forca");
 const agi = document.querySelector("#val-agilidade");
 const int = document.querySelector("#val-inteligencia");
@@ -78,3 +65,33 @@ btn_inc.forEach(btn =>{
         }
     })
 })
+
+formulario.addEventListener("submit", () => {
+    event.preventDefault();
+
+    const user = {
+        nome: document.querySelector("#nome").value,
+        raca: document.querySelector("#raca").value,
+        classe: document.querySelector("#classe").value,
+        forca: Number(document.querySelector("#val-forca").textContent),
+        velocidade: Number(document.querySelector("#val-agilidade").textContent),
+        inteligencia: Number(document.querySelector("#val-inteligencia").textContent)
+    };
+
+        listaUsers.innerHTML += `
+        <div class="card mt-3 p-3">
+            <h3>${user.nome}</h3>
+            <p>Raça: ${user.raca}</p>
+            <p>Classe: ${user.classe}</p>
+            <p>Força: ${user.forca}</p>
+            <p>agilidadde: ${user.velocidade}</p>
+            <p>inteligencia: ${user.inteligencia}</p>
+        </div>
+    `;
+    formulario.reset();
+    pontoRestante = 10;
+    pontosR.textContent = "Pontos restantes: 10";
+    forca.textContent = 0;
+    agi.textContent = 0;
+    int.textContent = 0;
+});
