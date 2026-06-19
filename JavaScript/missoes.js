d980553e4f373c7fa7aee2a02eb9c988f70354e5
Augusto Cezar Morgenstern <augusto_schaefer@estudante.sesisenai.org.br>
2026-06-19 15:12:42 -0300

    js de missoes pronto

--- JavaScript/missoes.js
+++ JavaScript/missoes.js
@@ -1,6 +1,23 @@
-const missoes =[
+const formulario = document.querySelector("#formulario");
+const listaUsers = document.querySelector("#lista-users");
 
-]
+formulario.addEventListener("submit", () => {
+    event.preventDefault();
 
+    const user = {
+        nome: document.querySelector("#nome").value,
+        raca: document.querySelector("#raca").value,
+        fraqueza: document.querySelector("#fraqueza").value,
+        localizacao: document.querySelector("#localizacao").value,
+    };
 
-console.log()
+        listaUsers.innerHTML += `
+        <div class="card mt-3 p-3">
+            <h3>${user.nome}</h3>
+            <p>Raça: ${user.raca}</p>
+            <p>fraqueza: ${user.fraqueza}</p>
+            <p>localização: ${user.localizacao}</p>
+        </div>
+    `;
+    formulario.reset();
+});
