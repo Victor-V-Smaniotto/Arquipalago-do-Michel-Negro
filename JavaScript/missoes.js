cab1ff72139a5c7ba721fffc0b67cd5052d71e93
AUGUSTO CEZAR MORGENSTERN SCHAEFER <augusto_schaefer@estudante.sesisenai.org.br>
2026-06-23 19:18:53 -0300

    botão de excluir pronto

--- JavaScript/missoes.js
+++ JavaScript/missoes.js
@@ -1,10 +1,14 @@
 const formulario = document.querySelector("#formulario");
 const listaMissoes = document.querySelector("#lista-missoes");
 
-const missoesSalvas = JSON.parse(localStorage.getItem("missoes")) || [];
 
-missoesSalvas.forEach(missao => {
-    listaMissoes.innerHTML += `
+function renderizarMissao(){
+    listaMissoes.innerHTML = "";
+
+    const missoes = JSON.parse(localStorage.getItem("missoes")) || [];
+
+    missoes.forEach((missao, index) => {
+        listaMissoes.innerHTML += `
         <div class="card mt-3 p-3">
             <img src="${missao.monstro}" alt="Imagem do monstro" class="img-fluid">
             <h5>Nome: ${missao.nome}</h5>
@@ -12,8 +16,30 @@ missoesSalvas.forEach(missao => {
             <h6>Perigo: ${missao.perigo}</h6>
             <h6>Localização: ${missao.localizacao}</h6>
             <h6>Recompensa: ${missao.recompensa}</h6>
+            <button class="btn btn-danger btn-excluir mt-2" data-index="${index}">Excluir</button>
         </div>
     `;
+    });
+};
+
+function deletarCadastro(index){
+    const missoes = JSON.parse(localStorage.getItem("missoes")) || [];
+
+    missoes.splice(index, 1);
+
+    localStorage.setItem("missoes", JSON.stringify(missoes));
+
+    renderizarMissao();
+}
+
+listaMissoes.addEventListener("click", (event) => {
+
+    if(event.target.classList.contains("btn-excluir")) {
+
+        const index = event.target.dataset.index;
+
+        deletarCadastro(index);
+    }
 });
 
 formulario.addEventListener("submit", (event) => {
@@ -48,5 +74,6 @@ formulario.addEventListener("submit", (event) => {
         </div>
     </div>
     `;
+    renderizarMissao();
     formulario.reset();
 });
