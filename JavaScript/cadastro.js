25b554da5a2af6431316e73409c446ffeedece6e
AUGUSTO CEZAR MORGENSTERN SCHAEFER <augusto_schaefer@estudante.sesisenai.org.br>
2026-06-22 22:08:19 -0300

    cadastro funfando

--- JavaScript/cadastro.js
+++ JavaScript/cadastro.js
@@ -6,27 +6,53 @@ const forca = document.querySelector("#val-forca");
 const agi = document.querySelector("#val-agilidade");
 const int = document.querySelector("#val-inteligencia");
 
-const feedback = document.querySelector("#feedback");
+
 const btn_dec = document.querySelectorAll(".btn-dec");
 const btn_inc = document.querySelectorAll(".btn-inc");
 const btn_exc = document.querySelectorAll(".btn-excluir");
 
 let pontoRestante = 10;
 
-const personagensSalvos = JSON.parse(localStorage.getItem("personagens")) || [];
 
-personagensSalvos.forEach(user, index => {
-    listaUsers.innerHTML += `
-    <div class="card mt-3 p-3">
-        <h3>${user.nome}</h3>
-        <h4>Raça: ${user.raca}</h4>
-        <h4>Classe: ${user.classe}</h4>
-        <h4>Força: ${user.forca}</h4>
-        <h4>Agilidade: ${user.velocidade}</h4>
-        <h4>Inteligência: ${user.inteligencia}</h4>
-        <button type="button" class="btn-excluir" data-index="${index}">excluir</button>
-    </div>
-    `;
+
+function renderizarCadastro(){
+    listaUsers.innerHTML = "";
+
+    const personagensSalvos = JSON.parse(localStorage.getItem("personagens")) || [];
+
+    personagensSalvos.forEach((user, index) => {
+        listaUsers.innerHTML += `
+        <div class="card mt-3 p-3">
+            <h3>${user.nome}</h3>
+            <h4>Raça: ${user.raca}</h4>
+            <h4>Classe: ${user.classe}</h4>
+            <h4>Força: ${user.forca}</h4>
+            <h4>Agilidade: ${user.velocidade}</h4>
+            <h4>Inteligência: ${user.inteligencia}</h4>
+            <button type="button" class="btn-excluir" data-index="${index}">excluir</button>
+        </div>
+        `;
+    });
+};
+
+function deletarCadastro(index){
+    const personagensSalvos = JSON.parse(localStorage.getItem("personagens")) || [];
+
+    personagensSalvos.splice(index, 1);
+
+    localStorage.setItem("personagens", JSON.stringify(personagensSalvos));
+
+    renderizarCadastro();
+}
+
+listaUsers.addEventListener("click", (event) => {
+
+    if(event.target.classList.contains("btn-excluir")) {
+
+        const index = event.target.dataset.index;
+
+        deletarCadastro(index);
+    }
 });
 
 console.log(pontoRestante);
@@ -85,7 +111,6 @@ btn_inc.forEach(btn =>{
 
 formulario.addEventListener("submit", (event) => {
     event.preventDefault();
-
     const user = {
         nome: document.querySelector("#nome").value,
         raca: document.querySelector("#raca").value,
@@ -95,33 +120,19 @@ formulario.addEventListener("submit", (event) => {
         inteligencia: Number(document.querySelector("#val-inteligencia").textContent)
     };
 
+
     const personagens = JSON.parse(localStorage.getItem("personagens")) || [];
-
     personagens.push(user);
-
     localStorage.setItem("personagens", JSON.stringify(personagens));
 
-    listaUsers.addEventListener("click", (event) =>{
-        localStorage.removeItem("personagensSalvos");
-    });
 
-    listaUsers.innerHTML += `
-    <div class="card mt-3 p-3">
-        <h3>${user.nome}</h3>
-        <h4>Raça: ${user.raca}</h4>
-        <h4>Classe: ${user.classe}</h4>
-        <h4>Força: ${user.forca}</h4>
-        <h4>Agilidade: ${user.velocidade}</h4>
-        <h4>Inteligência: ${user.inteligencia}</h4>
-        <button type="button" class="btn-excluir" data-index="${index}" >excluir</button>
-    </div>
-    `;
+    renderizarCadastro();
     formulario.reset();
 
     pontoRestante = 10;
     pontosR.textContent = "Pontos restantes: 10";
-
     forca.textContent = 0;
     agi.textContent = 0;
     int.textContent = 0;
 });
+renderizarCadastro();
